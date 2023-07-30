99a49c9e2c28a6dd4897a3f6277d894f7de89c08
notant <84870445+antbag-dev@users.noreply.github.com>
2023-07-31 01:32:39 +0500

    Update Loader.php

--- src/shelly7w7/TotemJoin/Loader.php
+++ src/shelly7w7/TotemJoin/Loader.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace shelly7w7\TotemJoin;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Loader extends PluginBase {
 
@@ -12,7 +13,7 @@ class Loader extends PluginBase {
 
 	public function onEnable(): void {
 		self::$instance = $this;
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 
 		$config = $this->getConfig();
 		$config->reload();
